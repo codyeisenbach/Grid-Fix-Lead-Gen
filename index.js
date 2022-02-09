ce466d1dc43f73b58ad3a23ba5cdd110d8906c23
J Cody Eisenbach <ceisenbach@gmail.com>
2022-02-09 00:24:54 -0600

    css mobile

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // Dependencies
 // =============================================================
 require("dotenv").config();
-const { JSDOM } = require("jsdom");
-const { window } = new JSDOM("");
-const $ = require("jquery")(window);
+// const { JSDOM } = require("jsdom");
+// const { window, document } = new JSDOM("");
+// const $ = require("jquery")(window);
 const nodemailer = require("nodemailer");
 var express = require("express");
 var path = require("path");
@@ -14,32 +14,36 @@ var PORT = process.env.PORT || 3000;
 // Dependencies
 // =============================================================
 
-/* resize the image(s) on page resize */
-$(window).on("resize", function () {
-  resize_all_parallax();
-});
+// $(document).ready(function () {
+//   resize_all_parallax();
+// });
 
-/* keep all of your resize function calls in one place so you don't have to edit them twice (on page load and resize) */
-function resize_all_parallax() {
-  var div_id = "img1"; /* the ID of the div that you're resizing */
-  var img_w = 1000; /* the width of your image, in pixels */
-  var img_h = 864; /* the height of your image, in pixels */
-  resize_parallax(div_id, img_w, img_h);
-}
+// /* resize the image(s) on page resize */
+// $(window).on("resize", function () {
+//   resize_all_parallax();
+// });
 
-/* this resizes the parallax image down to an appropriate size for the viewport */
-function resize_parallax(div_id, img_w, img_h) {
-  var div = $("#" + div_id);
-  var divwidth = div.width();
-  if (divwidth < 769) {
-    var pct = (img_h / img_w) * 105;
-  } /* show full image, plus a little padding, if on static mobile view */ else {
-    var pct = 60;
-  } /* this is the HEIGHT as percentage of the current div WIDTH. you can change it to show more (or less) of your image */
-  var newheight = Math.round(divwidth * (pct / 100));
-  newheight = newheight + "px";
-  div.height(newheight);
-}
+// /* keep all of your resize function calls in one place so you don't have to edit them twice (on page load and resize) */
+// function resize_all_parallax() {
+//   var div_id = "img1"; /* the ID of the div that you're resizing */
+//   var img_w = 1000; /* the width of your image, in pixels */
+//   var img_h = 864; /* the height of your image, in pixels */
+//   resize_parallax(div_id, img_w, img_h);
+// }
+
+// /* this resizes the parallax image down to an appropriate size for the viewport */
+// function resize_parallax(div_id, img_w, img_h) {
+//   var div = $("#" + div_id);
+//   var divwidth = div.width();
+//   if (divwidth < 769) {
+//     var pct = (img_h / img_w) * 105;
+//   } /* show full image, plus a little padding, if on static mobile view */ else {
+//     var pct = 60;
+//   } /* this is the HEIGHT as percentage of the current div WIDTH. you can change it to show more (or less) of your image */
+//   var newheight = Math.round(divwidth * (pct / 100));
+//   newheight = newheight + "px";
+//   div.height(newheight);
+// }
 
 // Routes
 // =============================================================
